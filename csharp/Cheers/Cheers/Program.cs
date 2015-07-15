aa6e90b04d09bf7dd1b64d39570556410d67494f
LandoB <jrolandobernal@gmail.com>
2015-07-14 21:58:12 -0500

    Got the cheering going with all the letters of the name. Need to figure out the an or a depending on the letter of the name. Working on it now

---
+++ csharp/Cheers/Cheers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Cheers
+{
+	class MyMainClass
+	{
+		public static void Main (string[] args)
+		{
+			// Prompt user to type a name
+			Console.Write("Please enter your name: ");
+			string input = Console.ReadLine();
+			input = input.ToLower();
+
+
+			// print out the cheer
+			// for each character in the name
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				Console.WriteLine("Give me a..{0}", input[i]);
+			}
+
+			// print out final line with name in uppercase
+			Console.WriteLine("{0} is.. GRANDIOSO!!!", input.ToUpper());
+
+			Console.ReadLine();
+		}
+	}
+}
+
+
+
