b0bf9710e6804ea7124468a6f0068f4973263d59
LandoB <jrolandobernal@gmail.com>
2015-07-14 23:32:17 -0500

    Added the rest of the letters that use AN

--- csharp/Cheers/Cheers/Program.cs
+++ csharp/Cheers/Cheers/Program.cs
@@ -7,29 +7,32 @@ namespace Cheers
 		public static void Main (string[] args)
 		{
 			string cheering; // It will determine if we need "a" or "an"
-			char[] vowels = new char[5] { 'a', 'e', 'i', 'o', 'u' }; // We are capturing the array of vowels to compare with the characters of the name
-																	 // if it is a vowel, then we'll use "an"
+			// We are capturing the array of characters that usually use "an" 
+			// then we will compare them with the characters of the name
+			// if it is one of them, then we'll use "an"
+			char[] an_characters = new char[13] { 'a', 'e', 'i', 'o', 'u', 'f', 'h', 'l', 'm', 'n', 'r', 's', 'x' }; 
+																	 
 
-			// Ask the user to type a name
-			Console.Write("Please enter your name: ");
-			string input = Console.ReadLine();
-			input = input.ToLower();
+			// We now ask the user to type a name
+			Console.Write("Please type your name: ");
+			string name = Console.ReadLine();
+			name = name.ToLower();
 
 			// Then, print out the cheering
 			// for each character in the name
-			// And we also compare it with our array of vowels
+			// And we also compare it with our array of "an" characters
 			// to see if we need "a" or "an" 
 			// then print out the line for the appropriate cheering
 
-			for (int i = 0; i < input.Length; i++)
+			for (int i = 0; i < name.Length; i++)
 			{
-				// Check to see if the character is a vowel or not
-				cheering = Array.IndexOf(vowels, input[i]) < 0 ? "a" : "an";
-				Console.WriteLine("Give me {0}..{1}", cheering, input[i]);
+				// Check to see if the character is a "an" character or not
+				cheering = Array.IndexOf(an_characters, name[i]) < 0 ? "a" : "an";
+				Console.WriteLine("Give me {0}..{1}", cheering, name[i]);
 			}
 
 			// Finally, print out the line with name in all uppercase
-			Console.WriteLine("{0} is.. GRANDIOSO!!!", input.ToUpper());
+			Console.WriteLine("{0} is.. GRANDIOSO!!!", name.ToUpper());
 
 			Console.ReadLine();
 		}
